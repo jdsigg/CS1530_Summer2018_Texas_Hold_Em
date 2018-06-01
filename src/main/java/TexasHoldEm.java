c34e1bfdb1d3502820d97e2add27814bf89a2b77
babriatis <bra29@pitt.edu>
2018-06-01 18:11:15 -0400

    Initialized gameboard, deck, and AiPlayer ArrayList

--- src/main/java/TexasHoldEm.java
+++ src/main/java/TexasHoldEm.java
@@ -2,6 +2,15 @@ public class TexasHoldEm
 {
 	public static void main(String[] args)
 	{
+		//Initialize the gameboard and deck
+		GameBoard table = new GameBoard();
+		//Deck dealerDeck = new Deck();
 		
+		//Get needed info from GUI class to set up the gameboard
+		int numOfOpponents = table.getNumberOfComputers;
+		String humanName = table.getHumanName;
+		
+		//Initialize an ArrayList with the number of opponents
+		ArrayList<AiPlayer> opponents = new ArrayList<>(numOfOpponents);
 	}
 }
