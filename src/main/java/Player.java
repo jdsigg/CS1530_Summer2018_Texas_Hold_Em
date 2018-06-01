23353c760d5e579da550ae01fe5dc6fae621bc2f
Antonino Febbraro <adf47@pitt.edu>
2018-05-31 20:48:53 -0400

    Started working on player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,41 @@
-//Insert code here
+class Player
+{
 
+  /*
+    Initilize Variables
+  */
+  String name;
+  double money;
+  Card [] currentHand = new Card[2];
+
+	public Player(String name)
+	{
+    //Constructor for Player
+    this.name = name;
+    this.money = 1000.00;
+	}
+
+  /*
+    Method to get player's stack (Money)
+  */
+  public double getMoney()
+  {
+    return this.money;
+  }
+
+  /*
+    Method to get player's name
+  */
+  public String getName()
+  {
+    return this.name;
+  }
+
+  /*
+    Method to update player's stack (Money)
+  */
+  public void updateMoney(double money)
+  {
+    this.money = money;
+  }
+}
