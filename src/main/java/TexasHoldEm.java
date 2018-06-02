7f87c610bb5ce74113507e8de5c8f1efca9f9b24
babriatis <bra29@pitt.edu>
2018-06-01 21:40:04 -0400

    Initialized all classes

--- src/main/java/TexasHoldEm.java
+++ src/main/java/TexasHoldEm.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
 
 public class TexasHoldEm
 {
@@ -9,11 +11,32 @@ public class TexasHoldEm
 		Deck dealerDeck = new Deck();
 		Dealer bob = new Dealer(dealerDeck);
 		
+		ArrayList<String> names = new ArrayList<>(Arrays.asList("Jacques Strap", 
+		"Seymour Butz", "Mike Rotch", "Ollie Tabooger", "Al Coholic", 
+		"Oliver Clothesoff", "Libby Doe", "Sarah Nader", "Robin Banks", "Hugh Jeffencock"));
+		
 		//Get needed info from GUI class to set up the gameboard
 		int numOfOpponents = table.getNumberOfComputers;
 		String humanName = new String(table.getHumanName);
 		
 		//Initialize an ArrayList with the number of opponents
 		ArrayList<Player> opponents = new ArrayList<>(numOfOpponents);
+		
+		//Shuffle the names of opponents to keep things fresh
+		Collections.shuffle(names);
+		
+		//Give opponents names
+		for (int i=0; i<numOfOpponents; i++)
+		{
+			Player x = new Player(names.get(i));
+			opponents.add(x);
+		}
+		
+		/*Test to make sure names are being added to the opponents ArrayList
+		
+		for (Player x: opponents){
+			System.out.println(x.getName());
+		}
+		*/
 	}
 }
